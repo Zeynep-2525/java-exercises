8b8825566278f3662c70ce9cd2a63afa51530880
Zeynep-2525 <zezetopal.zeynep@gmail.com>
2025-04-17 20:27:58 +0300

    Add Java exception handling example

---
+++ ExceptionExample.java
@@ -0,0 +1,12 @@
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println(numbers[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Hata yakalandı: " + e.getMessage());
+        } finally {
+            System.out.println("Program bitti.");
+        }
+    }
+}
